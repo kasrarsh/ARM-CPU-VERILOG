825c4c5796f66bdbf16d669d64a3dc957880dad1
kasrarsh <kasramagers1377@gmail.com>
2019-12-29 23:15:08 +0330

    fix alu error and dataMemory

--- dataMemmory.v
+++ dataMemmory.v
@@ -1,19 +1,17 @@
 module dataMemory
-(address,write_data,read_data,mem_write,mem_read);
+(address,write_data,read_data,mem_write,mem_read,clock);
 input [4:0] address;
 input [3:0] write_data;
-input mem_write;
+input mem_write , mem_read , clock;
 output [3:0] read_data;
 
 reg [3:0] memory [63:0];
-always@(posedge clock)
-	if(mem_write)
-		memory[address] <= write_data;
-end
-always@(posedge clock)
-	if(mem_read)
-		read_data <= memory[address];
-end
+always@(posedge clock) 
+	begin
+		if(mem_write)
+			memory[address] <= write_data;
+	end
+	assign read_data = memory[address];
 endmodule
 
 
